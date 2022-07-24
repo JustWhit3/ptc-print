5234e992a2d6b3dbb83b0c388895ce1be8b6a8a9
Carl Timmer <timmer@jlab.org>
2022-07-24 13:47:41 +0000

    Update docs

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_28_29_6',['operator()',['../classptc_1_1____print____.html#a642637f48b4eb8ddf3f8b5e860751fc4',1,'ptc::__print__::operator()(T_os &amp;os, const T &amp;first, const Args &amp;... args) const'],['../classptc_1_1____print____.html#ac71c1eda7463a35fcf4bc74e0577ed47',1,'ptc::__print__::operator()(const T &amp;first=&quot;&quot;, const Args &amp;... args) const'],['../classptc_1_1____print____.html#a902858d51792caa7278bf2df7a269a92',1,'ptc::__print__::operator()(std::ostream &amp;os=std::cout) const']]]
+  ['operator_28_29_6',['operator()',['../classptc_1_1____print____.html#a642637f48b4eb8ddf3f8b5e860751fc4',1,'ptc::__print__::operator()(T_os &amp;os, const T &amp;first, const Args &amp;... args) const'],['../classptc_1_1____print____.html#a5e94977636b15b674853e6503f62f7da',1,'ptc::__print__::operator()(const T &amp;first, const Args &amp;... args) const'],['../classptc_1_1____print____.html#a902858d51792caa7278bf2df7a269a92',1,'ptc::__print__::operator()(std::ostream &amp;os=std::cout) const']]]
 ];
