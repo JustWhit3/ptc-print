3f1be08bbe8ce4c337a9ce1695c98b802db1e531
Carl Timmer <timmer@jlab.org>
2022-07-25 13:44:30 +0000

    Update docs

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_gnopsv~",
-  1: "_n",
-  2: "p",
-  3: "p",
-  4: "_gos~",
-  5: "npv"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables"
 };
 
