3e48f9a88391a455f666ec34a3e312bcfc523f34
Carl Timmer <timmer@jlab.org>
2022-08-07 12:29:31 +0000

    Update docs

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_28_29_4',['operator()',['../structptc_1_1Print.html#a42d1c836d75c4d53bb06f73dbb0be457',1,'ptc::Print::operator()(T &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#a5bd94beb0d274d8181c40c6bea0c85fa',1,'ptc::Print::operator()(mode &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#acf8b739f183ea4ef3883bc9b33896a02',1,'ptc::Print::operator()(std::ostream &amp;os=std::cout) const']]]
+  ['operator_28_29_4',['operator()',['../structptc_1_1Print.html#a38dace1f19a1774cd984d8b87cefef29',1,'ptc::Print::operator()(T &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#a5bd94beb0d274d8181c40c6bea0c85fa',1,'ptc::Print::operator()(mode &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#acf8b739f183ea4ef3883bc9b33896a02',1,'ptc::Print::operator()(std::ostream &amp;os=std::cout) const']]]
 ];
