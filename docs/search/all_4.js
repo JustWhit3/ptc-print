f4cd44a44f9928a8970c0cdcfcdf844bdde61cdf
Carl Timmer <timmer@jlab.org>
2022-08-09 18:20:12 +0000

    Update docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['operator_28_29_7',['operator()',['../structptc_1_1Print.html#a38dace1f19a1774cd984d8b87cefef29',1,'ptc::Print::operator()(T &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#a5bd94beb0d274d8181c40c6bea0c85fa',1,'ptc::Print::operator()(mode &amp;&amp;first, Args &amp;&amp;... args) const'],['../structptc_1_1Print.html#acf8b739f183ea4ef3883bc9b33896a02',1,'ptc::Print::operator()(std::ostream &amp;os=std::cout) const']]],
-  ['operator_3c_3c_8',['operator&lt;&lt;',['../namespaceptc.html#a968610c95b81726e44a28e98a4f83e20',1,'ptc']]]
+  ['operator_3c_3c_8',['operator&lt;&lt;',['../namespaceptc.html#abb80c82a33f0d1de0459023fd95304f5',1,'ptc']]]
 ];
