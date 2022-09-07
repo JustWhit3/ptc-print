949d15cad0a4e60e7a8934c8b344d62b6be85214
Carl Timmer <timmer@jlab.org>
2022-09-07 13:35:14 +0000

    Update docs

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -6,5 +6,5 @@ var searchData=
   ['setpattern_24',['setPattern',['../structptc_1_1Print.html#afc8125add122e4749e7e2a11ef576805',1,'ptc::Print']]],
   ['setsep_25',['setSep',['../structptc_1_1Print.html#a6e806a4699367ece3b537cc4bc3153f2',1,'ptc::Print']]],
   ['str_26',['str',['../namespaceptc.html#aa730094183bf13993032c00ee196eafba341be97d9aff90c9978347f66f945b77',1,'ptc']]],
-  ['stringconverter_27',['StringConverter',['../namespaceptc.html#a69fe76695e910153bae4928621eeb099',1,'ptc']]]
+  ['stringconverter_27',['StringConverter',['../namespaceptc.html#a4e800f408c636fa99b5a95109cef63b4',1,'ptc']]]
 ];
